322f4963cda5207e7e4ded63295120ad66a95cd8
DimitrisMalamas <dimitrismal5595@gmail.com>
2016-10-30 21:53:03 +0200

    Update urls.py

--- fcga/urls.py
+++ fcga/urls.py
@@ -6,6 +6,9 @@ app_name = 'fcga'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^cdeck/$', views.cdeck, name='cdeck'),
+    url(r'^deck/(?P<deck_id>[0-9]+)/$', views.deck, name='deck'),
+    url(r'^play/(?P<deck_id>[0-9]+)/$', views.play, name='play'),
     url(r'^user/(?P<user_id>[0-9]+)/$', views.profile, name='profile'),
     url(r'^user/$', views.profile, name='profile'),
     url(r'^login/$', views.login_view, name='login'),
